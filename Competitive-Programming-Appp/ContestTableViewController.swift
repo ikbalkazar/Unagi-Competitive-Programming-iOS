3f77e7a32efccac7c42aa5b3d981f5071544dd61
Harun Gunaydin <harungunaydin@Harun-2.local>
2016-02-20 00:00:16 -0500

    Added new Slider View

--- Competitive-Programming-Appp/ContestTableViewController.swift
+++ Competitive-Programming-Appp/ContestTableViewController.swift
@@ -99,15 +99,14 @@ class ContestTableViewController: UITableViewController {
     }
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
         loadContests()
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Competitive-Programming-Appp/String.swift
@@ -0,0 +1,23 @@
+//
+//  StringExtension.swift
+//  SlideMenuControllerSwift
+//
+//  Created by Yuji Hato on 1/22/15.
+//  Copyright (c) 2015 Yuji Hato. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    static func className(aClass: AnyClass) -> String {
+        return NSStringFromClass(aClass).componentsSeparatedByString(".").last!
+    }
+    
+    func substring(from: Int) -> String {
+        return self.substringFromIndex(self.startIndex.advancedBy(from))
+    }
+    
+    var length: Int {
+        return self.characters.count
+    }
+}
