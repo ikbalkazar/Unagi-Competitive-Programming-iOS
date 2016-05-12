3fa8c4723f7d0c27d4f0dc1c5ff23d4ebbe3cdd8
Ikbal Kazar <mikbalkazar@gmail.com>
2016-05-12 21:03:29 +0300

    _

--- notificationBase/checkRatingUpdates.py
+++ notificationBase/checkRatingUpdates.py
@@ -1,11 +1,13 @@
 import httplib, urllib2
 
+#downloads the page with the given url and returns it's content in string format
 def getPage(url):
   request = urllib2.Request(url)
   response = urllib2.urlopen(request)
   page = str(response.read())
   return page
 
+#returns the codeforces nickname of the given contest's winner
 def getWinner(contestId):
 
   page = getPage("http://codeforces.com/contest/%d/standings" % contestId)
@@ -26,26 +28,28 @@ def getWinner(contestId):
       if s[i:].startswith("<a href="):
         return getTillQuote(s[i+9:])
 
-  profileUrl = ""
   found = False
+  at = page.index(lookFor)
 
-  for i in xrange(len(page)):
-    if page[i:i+500].startswith(lookFor):
-      profileUrl = getProfileUrl(page[i:i+500])
-      found = True
-      break
-
-  assert(found)
+  if at >= 0:
+    found = True
+    profileUrl = getProfileUrl(page[at:at+500])
+ 
+  if found == False:
+    raise Exception("No winner found")
   return profileUrl[9:]
 
 contestId = input()
 
 winner = getWinner(contestId)
 
+print winner
+
 contestsUrl = "http://codeforces.com/contests/with/" + winner
 
 contestsPage = getPage(contestsUrl)
 
+#checks whether rating information of the contest exists on the winner's profile
 if contestsPage.index("/contest/%d" % contestId) >= 0:
   print "Ratings are out!!"
 else:
--- notificationBase/checkStatus.py
+++ notificationBase/checkStatus.py
@@ -1,9 +1,11 @@
 import urllib, urllib2, httplib
 
+#downloads the status page of the given contest
 contestId = input()
 request = urllib2.Request("http://codeforces.com/contest/%d/status" % contestId)
 response = urllib2.urlopen(request)
 htmlFile = str(response.read())
 
+#looks for "Finished" to check whether system testing is 100% done.
 if htmlFile.index("Finished") >= 0:
   print "Finished"
